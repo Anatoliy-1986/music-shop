ff5f927bf65d6e35a6d7d6f9ac07e4e3742bfa2d
Anatoliy-1986 <Gospodarikov_aa@mail.ru>
2023-03-09 17:31:17 +0300

    added slider page

---
+++ components/sliderCollection/slider/useSlider.tsx
@@ -0,0 +1,34 @@
+import { useState } from "react";
+
+export const useSlider = (arrLenght: number) => {
+  const [slide, setSlide] = useState(1);
+
+  const nextSlide = () => {
+    if (slide < arrLenght - 1) {
+      return setSlide((prev) => (prev += 1));
+    }
+    setSlide(() => 0);
+  };
+
+  const previousSlide = () => {
+    if (slide > 0) {
+      return setSlide((prev) => (prev -= 1));
+    }
+    setSlide(() => arrLenght - 1);
+  };
+
+  const slideName = (index: number) => {
+    switch (true) {
+      case index === slide:
+        return "active";
+
+      case index === slide - 1 || (slide === 0 && index === arrLenght - 1):
+        return "last";
+
+      default:
+        return "next";
+    }
+  };
+
+  return { slide, nextSlide, previousSlide, slideName };
+};
